b79d3ed9f20bdd38a28d4e0bccee962eb22d2de3
HerePed <aaa88456@hotmail.com>
2018-09-21 03:31:21 +0700

    update home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './Assets/logo.png';
+import BG from './Assets/rightH.png';
 import './App.css';
+import { Columns,Button } from 'react-bulma-components/full';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  render(){
+    return(
+      <Columns>
+        <Columns.Column className="columns is-desktop is-mobile is-tablet is-widescreen is-fullhd">
+          <div className="left">
+          <img className="LG" src={logo}/>
+          <p className="para">Edudy school of the future</p>
+          </div>
+        </Columns.Column>
+        <Columns.Column className="columns is-desktop is-mobile is-tablet is-widescreen is-fullhd">
+          <div className="right">
+          <img className="pic" src={BG}/>
+          </div>
+        </Columns.Column>
+        <Columns.Column className="columns is-desktop is-mobile is-tablet is-widescreen is-fullhd">
+          <div className="center">
+          <button className="button is-rounded">Into Website</button>
+          </div>
+        </Columns.Column>
+      </Columns>
     );
   }
 }
-
 export default App;
